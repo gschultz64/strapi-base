194df4d58acc8d10d5d68e0538fd037493c7ac1d
Genevieve <genschultz@gmail.com>
2018-09-27 14:25:36 -0700

    worked on routes and components

---
+++ public/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="home">
+    <img src="assets/hineini.jpg" alt="Hineini"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,49 @@
-const app = new Vue({
-  el: '#app',
-  data: {
+"use strict";
 
-  },
-  methods: {
-
-  },
-  template: `
-    
+const comics = Vue.component("comics", {
+  props: ["comics"],
+  template: ` 
+    <div>
+      <h2>Comics</h2>
+    </div>
   `
-})
+});
+
+const art = Vue.component("art", {
+  props: ["art"],
+  template: `
+    <div>
+      <h2>Art</h2>
+    </div>
+  `
+});
+
+const about = Vue.component("about", {
+  props: ["about"],
+  template: `
+    <div><h2>About Jean</h2></div>
+  `
+});
+
+const router = new VueRouter({
+  routes: [
+    { path: "/comics", component: comics },
+    { path: "/art", component: art },
+    { path: "/about", component: about }
+  ]
+});
+
+const app = new Vue({
+  el: "#app",
+  data: {},
+  methods: {},
+  template: `
+    <div>
+      <img class="logo" src="assets/jeanlogo.jpg" alt="Jean Eglinton Logo"/>
+      <main>
+        <img id="home" src="assets/hineini.jpg" alt="Hineini"/>
+      </main>
+    </div>
+  `,
+  router
+});
